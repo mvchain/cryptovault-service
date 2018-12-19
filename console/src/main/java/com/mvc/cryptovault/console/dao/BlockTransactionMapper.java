c1e01da8ddb50a9e3a0f95373611a92cb637b225
戚羿辰 <416350144@qq.com>
2018-12-19 21:16:12 +0800

    fix block status bug

--- console/src/main/java/com/mvc/cryptovault/console/dao/BlockTransactionMapper.java
+++ console/src/main/java/com/mvc/cryptovault/console/dao/BlockTransactionMapper.java
@@ -7,7 +7,7 @@ import org.apache.ibatis.annotations.Update;
 
 public interface BlockTransactionMapper extends MyMapper<BlockTransaction> {
 
-    @Update("update block_transaction set status = 2,updated_at = #{current}, transaction_status = 4 where id = #{obj.id} and updated_at = #{obj.updatedAt}")
+    @Update("update block_transaction set status = 2,updated_at = #{current}, transaction_status = 5 where id = #{obj.id} and updated_at = #{obj.updatedAt}")
     int updateSuccess(@Param("obj") BlockTransaction obj, @Param("current") Long currentTimeMillis);
 
     @Update("update block_transaction set hash = #{hash},status=1,transaction_status=2,error_msg='',error_data='' where order_number = #{orderId}")
