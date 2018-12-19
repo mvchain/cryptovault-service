489cc32855d8e0876ffc8d26fcac397d11e48134
戚羿辰 <416350144@qq.com>
2018-12-19 17:17:44 +0800

    fix collect bug

--- console/src/main/java/com/mvc/cryptovault/console/dao/CommonAddressMapper.java
+++ console/src/main/java/com/mvc/cryptovault/console/dao/CommonAddressMapper.java
@@ -12,6 +12,6 @@ public interface CommonAddressMapper extends MyMapper<CommonAddress> {
     @Select("select * from common_address where used = 0 and token_type = #{tokenType} limit 1")
     CommonAddress findUnUsed(@Param("tokenType") String tokenType);
 
-    @Select("SELECT t1.* from common_address t1, common_token t2 WHERE t1.token_type = t2.token_name and t1.balance > t2.hold and t1.balance > t2.transafer_fee and user_id > 0 and t1.address_type != 'ETH'")
+    @Select("SELECT t1.* from common_address t1, common_token t2 WHERE t1.token_type = t2.token_name and t1.balance > t2.hold and t1.balance > t2.transafer_fee and user_id > 0 and t1.token_type = 'ETH'")
     List<CommonAddress> findColldect();
 }
