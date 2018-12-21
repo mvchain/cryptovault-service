0eabf66e697537ca83cb12678686f984447c76ed
戚羿辰 <416350144@qq.com>
2018-12-21 20:10:40 +0800

    fix eth transaction status bug

--- console/src/main/java/com/mvc/cryptovault/console/dao/BlockTransactionMapper.java
+++ console/src/main/java/com/mvc/cryptovault/console/dao/BlockTransactionMapper.java
@@ -12,7 +12,7 @@ public interface BlockTransactionMapper extends MyMapper<BlockTransaction> {
     @Update("update block_transaction set status = 2,updated_at = #{current}, transaction_status = 5 where id = #{obj.id} and updated_at = #{obj.updatedAt}")
     int updateSuccess(@Param("obj") BlockTransaction obj, @Param("current") Long currentTimeMillis);
 
-    @Update("update block_transaction set hash = #{hash},status=1,transaction_status=2,error_msg='',error_data='' where order_number = #{orderId}")
+    @Update("update block_transaction set hash = #{hash},status=1,transaction_status=4,error_msg='',error_data='' where order_number = #{orderId}")
     int updateHash(@Param("orderId") String orderId, @Param("hash") String hash);
 
     @Update("UPDATE app_user_balance t1, block_transaction t2 SET t1.balance = t1.balance + t2.`value` WHERE t1.token_id = t2.token_id AND t2.id IN (${ids}) AND t2.transaction_status = 3")
