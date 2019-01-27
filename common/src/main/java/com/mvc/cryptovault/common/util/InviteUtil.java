0ad219d2399524b93bf8ebbdd349398a7a4d58e8
戚羿辰 <416350144@qq.com>
2019-01-27 17:37:28 +0800

    change invite code method

--- common/src/main/java/com/mvc/cryptovault/common/util/InviteUtil.java
+++ common/src/main/java/com/mvc/cryptovault/common/util/InviteUtil.java
@@ -1,6 +1,5 @@
 package com.mvc.cryptovault.common.util;
 
-import java.math.BigInteger;
 import java.util.Random;
 
 /**
@@ -9,65 +8,74 @@ import java.util.Random;
  */
 public class InviteUtil {
 
-    /** 自定义进制(0,1没有加入,容易与o,l混淆) */
-    private static final char[] r=new char[]{'Q', 'W', 'E', '8', 'A', 'S', '2', 'D', 'Z', 'X', '9', 'C', '7', 'P', '5', 'I', 'K', '3', 'M', 'J', 'U', 'F', 'R', '4', 'V', 'Y', 'L', 'T', 'N', '6', 'B', 'G', 'H'};
+    /**
+     * 自定义进制(0,1没有加入,容易与o,l混淆)
+     */
+    private static final char[] r = new char[]{'Q', 'W', 'E', '8', 'A', 'S', '2', 'D', 'Z', 'X', '9', 'C', '7', 'P', '5', 'I', 'K', '3', 'M', 'J', 'U', 'F', 'R', '4', 'V', 'Y', 'L', 'T', 'N', '6', 'B', 'G', 'H'};
 
-    /** (不能与自定义进制有重复) */
-    private static final char b='o';
+    /**
+     * (不能与自定义进制有重复)
+     */
+    private static final char b = 'o';
 
-    /** 进制长度 */
-    private static final int binLen=r.length;
+    /**
+     * 进制长度
+     */
+    private static final int binLen = r.length;
 
-    /** 序列最小长度 */
-    private static final int s=6;
+    /**
+     * 序列最小长度
+     */
+    private static final int s = 6;
 
     /**
      * 根据ID生成六位随机码
+     *
      * @param id ID
      * @return 随机码
      */
-    public static String toSerialCode(long id) {
-        char[] buf=new char[32];
-        int charPos=32;
+    public static String toSerialCode(Long id) {
+        char[] buf = new char[32];
+        int charPos = 32;
 
-        while((id / binLen) > 0) {
-            int ind=(int)(id % binLen);
-            buf[--charPos]=r[ind];
+        while ((id / binLen) > 0) {
+            int ind = (int) (id % binLen);
+            buf[--charPos] = r[ind];
             id /= binLen;
         }
-        buf[--charPos]=r[(int)(id % binLen)];
-        String str=new String(buf, charPos, (32 - charPos));
+        buf[--charPos] = r[(int) (id % binLen)];
+        String str = new String(buf, charPos, (32 - charPos));
         // 不够长度的自动随机补全
-        if(str.length() < s) {
-            StringBuilder sb=new StringBuilder();
+        if (str.length() < s) {
+            StringBuilder sb = new StringBuilder();
             sb.append(b);
-            Random rnd=new Random();
-            for(int i=1; i < s - str.length(); i++) {
-                sb.append(r[rnd.nextInt(binLen)]);
+            Random rnd = new Random();
+            for (int i = 1; i < s - str.length(); i++) {
+                sb.append(r[(int) (id + i % binLen)]);
             }
-            str+=sb.toString();
+            str += sb.toString();
         }
         return str;
     }
 
     public static long codeToId(String code) {
-        char chs[]=code.toCharArray();
-        long res=0L;
-        for(int i=0; i < chs.length; i++) {
-            int ind=0;
-            for(int j=0; j < binLen; j++) {
-                if(chs[i] == r[j]) {
-                    ind=j;
+        char chs[] = code.toCharArray();
+        long res = 0L;
+        for (int i = 0; i < chs.length; i++) {
+            int ind = 0;
+            for (int j = 0; j < binLen; j++) {
+                if (chs[i] == r[j]) {
+                    ind = j;
                     break;
                 }
             }
-            if(chs[i] == b) {
+            if (chs[i] == b) {
                 break;
             }
-            if(i > 0) {
-                res=res * binLen + ind;
+            if (i > 0) {
+                res = res * binLen + ind;
             } else {
-                res=ind;
+                res = ind;
             }
             // System.out.println(ind + "-->" + res);
         }
